33fc184916b9cdd8f304c017a294bf9f84e56c3c
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-12-07 15:21:10 -0600

    added sql data and angular functionality to 2014 Game 7 example

--- public/appRoutes.js
+++ public/appRoutes.js
@@ -1,12 +1,15 @@
 angular
 	.module('appRoutes', ["ui.router"])
-	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-		$stateProvider.state({
-			name: 'baseball',
-			url: '/',
-			templateUrl: 'public/components/baseball/templates/baseball.template',
-			controller: 'MainController'
-	});
-
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: false
+		})
+		$stateProvider
+			.state('baseball', {
+				url: '/',
+				templateUrl: 'public/components/baseball/templates/baseball.template',
+				controller: 'MainController'
+			})
 	$urlRouterProvider.otherwise('/');
 }])
