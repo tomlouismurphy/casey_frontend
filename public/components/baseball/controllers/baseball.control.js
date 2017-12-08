1adf152b18025f7909e0a095c3481bd6f6850c98
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-12-08 03:56:45 -0600

    substituting players operational

--- public/components/baseball/controllers/baseball.control.js
+++ public/components/baseball/controllers/baseball.control.js
@@ -2,8 +2,37 @@ app
 	.controller('MainController', function($scope, Batter, Pitcher){
 		$scope.lineup = [];
 		$scope.bench = [];
+		$scope.pinchHitter = '';
+		$scope.playerSubstituted = '';
+		this.pinchHit = function(event){
+			$scope.pinchHitter = event.target.innerText;
+		}
+		this.pickSubstitute = function(event){
+			$scope.playerSubstituted = event.target.innerText;
+		}
 		this.addToLineup = function(event){
+			for (let i = 0; i < $scope.bench.length; i++){
+				if ($scope.bench[i] === event.target.innerText){
+					$scope.lineup.push($scope.bench[i]);
+					$scope.bench.splice(i, 1);
+					console.log($scope.lineup);
+				}
+			}
 			console.log(event.target.innerText);
+		}
+		this.authorizeReplace = function(){
+			for (let i = 0; i < $scope.lineup.length; i++){
+				if ($scope.lineup[i] === $scope.playerSubstituted){
+					$scope.lineup[i] = $scope.pinchHitter;
+				}
+			}
+			for (let i = 0; i < $scope.bench.length; i++){
+				if ($scope.bench[i] === $scope.pinchHitter){
+					$scope.bench.splice(i, 1);
+				}
+			}
+			$scope.pinchHitter = '';
+			$scope.playerSubstituted = '';
 		}
 		Batter.query().$promise.then(function(data) {
 			$scope.batters = data;
@@ -14,8 +43,6 @@ app
 					$scope.bench.push(data[i].name);
 				}
 			}
-			console.log($scope.lineup);
-			console.log($scope.bench);
 		});
 		Pitcher.query().$promise.then(function(data) {
 			$scope.pitchers = data;
